98b79cd9a8f3a6682c3e61585709a1a832996092
mrjamieson29 <157923825+mrjamieson29@users.noreply.github.com>
2024-04-02 10:01:36 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+/**
+ * ICS3UC - Mr. J 
+ *  
+ * Unit 1 Summative Task
+ * 
+ * Read the README.md file (right-click it and select Open Preview)
+ * Only start this after completing the quiz. 
+ * 
+ * Author: 
+ */
+
+'use strict';
+const prompt = require("prompt-sync")();
+
+// hidden_number is a random number from 1 to 50
+let hidden_number = Math.floor(Math.random() * 50 + 1);
